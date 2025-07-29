bdeb4d7ae56a38469dd4858f4d511ba8a80edc2f
yusufomercoskun <yusufomercoskun.f24@fmvisik.k12.tr>
2025-07-29 17:59:00 +0300

    Add files via upload

---
+++ YusufCase/YusufCase/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.Devices.Common;
+
+
+Random random = new Random();
+int randomSayi = random.Next(0, 100);
+string cevap;
+int cevapInt;
+int i = 1;
+Console.Write("oyun başlasın\n");
+Console.Write("sayıyı tahmin edin: ");
+
+while (true)
+{
+    cevap = Console.ReadLine();
+    cevapInt = Convert.ToInt16(cevap);
+
+    if (cevapInt == randomSayi)
+    {
+        Console.Write("cevap doğru. oyun bitti.");
+        Console.WriteLine(i.ToString() + ". denemede bildin.");
+        break;
+    }
+    else if (cevapInt < randomSayi)
+    {
+        Console.Write("\ndaha büyük sayı gir: ");
+    }
+    else
+    {
+        Console.Write("\ndaha küçük sayı gir: ");
+    }
+
+    i++;
+}
